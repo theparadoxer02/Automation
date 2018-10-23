ada3b5fc4fea1270361193690ce474df1dfedfd3
Mannu Gupta <abhimanyu98986@gmail.com>
2018-10-24 01:52:58 +0530

    minor modification

--- gmail_automation.py
+++ gmail_automation.py
@@ -7,26 +7,42 @@ browser = webdriver.Firefox()
 browser.get('http://www.gmail.com')
 
 
-email = browser.find_element_by_name('Email')           #email_element selection
-email.send_keys("ENTER EMAIL") 							#Enter Email                               
+email = browser.find_element_by_id('identifierId')           #email_element selection
+email.send_keys("pankajgoyal125@gmail.com") 							#Enter Email                               
+
+next_button = browser.find_element_by_class_name('RveJvd')
+next_button.click()
 
 browser.implicitly_wait(3)                              #wait_for 3 seconds
 
-next_step  = browser.find_element_by_name('signIn')     #signin_elemet selection
-next_step.click()
+
+# password = browser.find_element_by_class_name('zHQkBf')       #passsword element selection
+# password.click()
+
+password = browser.find_element_by_xpath("//input[@type='password']")
+password.send_keys('Pankaj@123')
+# browser.find_element_by_id("passwordNext").click()
+
+next_button.click()
 
 
-password = browser.find_element_by_name('Passwd')       #passsword element selection
+# password_next_button = browser.find_element_by_class_name('ZFr60d CeoRYc')
+# password_next_button.click()
+
+# next_step  = browser.find_element_by_name('signIn')     #signin_elemet selection
+# next_step.click()
 
 
-with open('test.txt', 'r') as myfile:   #Reads password from a text file 
-    Password=myfile.read().replace('\n', '')
-
-password.send_keys(Password)
 
 
-sign_in =  browser.find_element_by_css_selector('#signIn')  #sign_in selection
-sign_in.click()
+# with open('test.txt', 'r') as myfile:   #Reads password from a text file 
+#     Password=myfile.read().replace('\n', '')
+
+# password.send_keys(Password)
+
+
+# sign_in =  browser.find_element_by_css_selector('#signIn')  #sign_in selection
+# sign_in.click()
 
 browser.implicitly_wait(200)                              #wait for 5 seconds
 
